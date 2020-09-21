8350cfa41da210e39c5c0871143dd6b17f1f6f19
Krishna Bhandari <krishna_bhandari2014@outlook.com>
2020-09-21 16:00:56 +0545

    wip

--- src/Weight.php
+++ src/Weight.php
@@ -1,13 +1,13 @@
 <?php
 
 namespace Spatie\UnitConversions;
+use Exceptions;
 
 class Weight
 {
     private float $kilograms;
 
-    public static function fromKilograms(float $kilograms): self
-    {
+    public static function fromKilograms(float $kilograms): self{
         return new static($kilograms);
     }
 
